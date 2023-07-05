b030e8667ad1f3d83bd6a0c06032f24c98d1d43b
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-07-06 00:42:13 +0300

    minor + add enum serialization

--- lib/include/gls/date_time.hpp
+++ lib/include/gls/date_time.hpp
@@ -2,6 +2,7 @@
 namespace gls {
 class DateTime {
   std::chrono::milliseconds from_epoch;
+
  public:
   DateTime(std::chrono::milliseconds ms) : from_epoch(ms){};
   std::string AsString() { return date::format("%FT%TZ", from_epoch); }
--- lib/include/gls/models/calculation.hpp
+++ lib/include/gls/models/calculation.hpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <gls/date_time.hpp>
+#include <nlohmann/json.hpp>
 #include <string>
 namespace gls {
 struct TCalculation {
@@ -19,6 +20,21 @@ struct TCalculation {
     kQueueRecalculation,
     kIrrelevant
   };
+  NLOHMANN_JSON_SERIALIZE_ENUM(
+      CalculationStatus,
+      {
+          {CalculationStatus::kNew, "new"},
+          {CalculationStatus::kInQueue, "in_queue"},
+          {CalculationStatus::kCalculating, "calculating"},
+          {CalculationStatus::kSuccess, "success"},
+          {CalculationStatus::kError, "error"},
+          {CalculationStatus::kDesigned, "calculating"},
+          {CalculationStatus::kForRecalculation, "for_recalculation"},
+          {CalculationStatus::kRecalculation, "recalculation"},
+          {CalculationStatus::kCancel, "cancel"},
+          {CalculationStatus::kQueueRecalculation, "queue_recalculation"},
+          {CalculationStatus::kIrrelevant, "irrelevant"}
+      })
   CalculationStatus status;
   std::string updated_at;
   bool favorite;
