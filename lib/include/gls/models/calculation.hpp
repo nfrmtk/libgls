3606b71b4283957e5f95ed1687c3dda9e086bebe
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-07-05 21:19:14 +0300

    ci should pass

--- lib/include/gls/models/calculation.hpp
+++ lib/include/gls/models/calculation.hpp
@@ -1,8 +1,7 @@
 #include <string>
 #include <chrono>
-#include <date_time.hpp>
+#include <gls/date_time.hpp>
 namespace gls{
-
   struct TCalculation {
     std::int64_t id;
     std::int64_t project_id;
@@ -20,10 +19,15 @@ namespace gls{
       kQueueRecalculation,
       kIrrelevant
     };
+
     CalculationStatus status;
     DateTime updated_at;
     bool favorite;
     DateTime created_at;
     DateTime provisional_completion_date;
   };
+  static std::unordered_map<std::string,TCalculation::CalculationStatus> mappings{
+      //todo!
+  };
+
 }
