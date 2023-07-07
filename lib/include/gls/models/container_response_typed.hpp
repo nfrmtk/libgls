edbcc680bffb9d19bb29e73da3695f5bab4f220a
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-07-07 15:40:55 +0300

    some more + make format

---
+++ lib/include/gls/models/container_response_typed.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <cstdint>
+#include <nlohmann/json.hpp>
+#include <string>
+
+#include "cargo_space_list_enum.hpp"
+namespace gls {
+struct TContainerResponse {
+  std::string type;
+  std::int64_t id;
+  std::string title;
+  double mass;
+  double carrying_capacity;
+  std::int64_t length;
+  std::int64_t width;
+  std::int64_t height;
+  std::int64_t loading_width;
+  std::int64_t loading_height;
+  std::int64_t indentation_width;
+  std::int64_t indentation_width_right;
+  std::int64_t indentation_height;
+  std::int64_t indentation_height_top;
+  std::int64_t indentation_length;
+  std::int64_t indentation_length_end;
+  std::string created_at;
+  std::string updated_at;
+  bool demo;
+  std::int64_t user;
+  std::string type;
+  std::string uid;
+};
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(TContainerResponse, type, id, title, length,
+                                   width, height, mass, carrying_capacity,
+                                   loading_width, loading_height,
+                                   indentation_width, indentation_width_right,
+                                   indentation_height, indentation_height_top,
+                                   indentation_length, indentation_length_end,
+                                   created_at, updated_at, demo, user, type,
+                                   uid)
+}  // namespace gls
--- lib/include/gls/models/type_report_4e4_enum.hpp
+++ lib/include/gls/models/type_report_4e4_enum.hpp
@@ -2,12 +2,9 @@
 #include <nlohmann/json.hpp>
 #include <string>
 
-namespace gls
-{
-    enum class TypeReport4e4Enum
-    {
-        excel = 0,
-        pdf
-    };
-    NLOHMANN_JSON_SERIALIZE_ENUM(TypeReport4e4Enum, { {TypeReport4e4Enum::excel, "excel"}, {TypeReport4e4Enum::pdf, "pdf"} })
-}
+namespace gls {
+enum class TypeReport4e4Enum { excel = 0, pdf };
+NLOHMANN_JSON_SERIALIZE_ENUM(TypeReport4e4Enum,
+                             {{TypeReport4e4Enum::excel, "excel"},
+                              {TypeReport4e4Enum::pdf, "pdf"}})
+}  // namespace gls
